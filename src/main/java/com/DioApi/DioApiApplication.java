c6f3e93cc9a762b734814f260838d6dd7ab58d39
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2024-06-29 00:15:27 -0300

    Corrected CORS

--- src/main/java/com/DioApi/DioApiApplication.java
+++ src/main/java/com/DioApi/DioApiApplication.java
@@ -1,10 +1,14 @@
 package com.DioApi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default Server URL")})
 @SpringBootApplication
-public class DioApiApplication {
+public class
+DioApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DioApiApplication.class, args);
